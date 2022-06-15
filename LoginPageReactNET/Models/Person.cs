7e49836197786a2b01a95187780654910f0fc31c
leopapst@hotmail.com <leopapst@hotmail.com>
2022-06-14 23:51:29 -0300

    First commit/Added Model folder

---
+++ LoginPageReactNET/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoginPageReactNET.Models
+{
+    public class Person
+    {
+        [Table("Person")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
